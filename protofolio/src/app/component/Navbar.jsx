45349cda2c242ea28ee93b6e93d207272f937a3e
Xrtion <kicaubae@gmail.com>
2024-05-17 02:59:15 +0700

    update

--- protofolio/src/app/component/Navbar.jsx
+++ protofolio/src/app/component/Navbar.jsx
@@ -6,7 +6,7 @@ import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid';
 
 const navLinks = [
     {
-        title: "about",
+        title: "About",
         path: "#about"
     },
     {
@@ -21,36 +21,39 @@ const navLinks = [
 
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
-  
-    return (
-      <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-          <Link href='/' className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
-          <div className='mobile-menu block md:hidden'>
-            <button
-              onClick={() => setNavbarOpen(!navbarOpen)}
-              className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:border-white'
-            >
-              {navbarOpen ? (
-                <XMarkIcon className='h-5 w-5' />
-              ) : (
-                <Bars3Icon className='h-5 w-5' />
-              )}
-            </button>
-          </div>
-          <div className={`menu ${navbarOpen ? 'block' : 'hidden'} md:flex md:w-auto`} id='navbar'>
-            <ul className='flex flex-col md:flex-row p-4 md:p-0 md:space-x-8 mt-0'>
-              {navLinks.map((link, index) => (
-                <li key={index}>
-                  <NavLink href={link.path} title={link.title} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  };
-  
+    return ( 
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+            <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+                <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
+                <div className='mobile-menu blck md:hidden'>
+                {!navbarOpen ? (
+                  <button
+                    onClick={() => setNavbarOpen(true)}
+                    className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:border-white'
+                  >
+                    <Bars3Icon className='h-5 w-5' />
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => setNavbarOpen(false)}
+                    className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:border-white'
+                  >
+                    <XMarkIcon className='h-5 w-5' />
+                  </button>
+                )}
+                </div>
+                <div className='menu hidden md:block md:w-auto' id='navbar'>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                        <NavLink href={link.path} title={link.title}/>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar
--- protofolio/src/app/page.js
+++ protofolio/src/app/page.js
@@ -5,8 +5,8 @@ import Navbar from "./component/Navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div className="container mx-24 px-12 py-4"> 
+      <Navbar/>
+      <div className="container mt-24 px-12 py-4"> 
         <HeroSection/>
       </div>
     </main>
