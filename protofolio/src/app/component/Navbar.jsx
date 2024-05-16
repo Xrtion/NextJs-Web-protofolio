508180eedd61b99bc4b08d03cbffc75051765cc4
Xrtion <kicaubae@gmail.com>
2024-05-16 22:10:08 +0700

    updated

--- protofolio/src/app/component/Navbar.jsx
+++ protofolio/src/app/component/Navbar.jsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from 'next/link'
-import React from 'react'
+import React, { useState} from 'react'
 import NavLink from './Navlink'
+import {Bars3Icon, XnarkIcon} from '@heroicons/react/24/solid';
 
 const navLinks = [
     {
@@ -18,22 +20,26 @@ const navLinks = [
 ]
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className='flex- flex-wrap items-center justify-between mx-auto p-8'>
-            <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>D</Link>
-            <div className='menu hidden md:block md:w-auto' id='navbar'>
-                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+            <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+                <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
+                <div className='mobile-menu block md:hidden'>
+
+                </div>
+                <div className='menu hidden md:block md:w-auto' id='navbar'>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                     {navLinks.map((link, index) => (
                         <li key={index}>
-                            <NavLink href={link.path} title={link.title}/>
+                        <NavLink href={link.path} title={link.title}/>
                         </li>
                     ))}
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
