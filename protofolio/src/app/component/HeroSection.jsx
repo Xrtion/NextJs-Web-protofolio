955c52e9a2eccd7c7f34b2fe8f2c3b7c1dbf654a
Xrtion <kicaubae@gmail.com>
2024-05-16 21:09:48 +0700

    update

--- protofolio/src/app/component/HeroSection.jsx
+++ protofolio/src/app/component/HeroSection.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center">
-                <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I'm David </h1>
-                <p className='text-[#ABD7BE] text-lg lg:text-xl'>
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>Hello, I'm David </h1>
+                <p className='text-[#ABD7BE] text-base sm:text-lg mb-6 lg:text-xl'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, necessitatibus. Animi at, rerum expedita, ad, facilis tenetur saepe labore officiis voluptas iste perspiciatis obcaecati soluta deserunt facere. Sequi, vero accusantium?
                 </p>
+                <div>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+                </div>
             </div>
-            <div className="col-span-5">
-                
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
+                        src="/images/Pic1.png"
+                        alt='pic1.img'
+                        className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                        width={300}
+                        height={300}
+                    />
+                </div>
             </div>
         </div>
     </section>
