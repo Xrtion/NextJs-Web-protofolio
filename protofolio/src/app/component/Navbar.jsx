21d35656a9323b301fc338e34e10f3e2ad8cfab8
Xrtion <kicaubae@gmail.com>
2024-05-17 03:15:35 +0700

    Update

--- protofolio/src/app/component/Navbar.jsx
+++ protofolio/src/app/component/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                     <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                     {navLinks.map((link, index) => (
                         <li key={index}>
-                        <NavLink href={link.path} title={link.title}/>
+                          <NavLink href={link.path} title={link.title}/>
                         </li>
                     ))}
                     </ul>
